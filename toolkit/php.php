e782677d8fdf4a4eba63b656a15c99395c81a38c
Varun <varunsridharan23@gmail.com>
2020-10-23 12:22:07 +0530

    Updated

--- toolkit/php.php
+++ toolkit/php.php
@@ -6,5 +6,7 @@ require_once __DIR__ . '/php/inputvar.php';
 require_once __DIR__ . '/php/env.php';
 
 function gh_gitconfig( $username = 'Github Action Bot', $email = 'githubactionbot@gmail.com' ) {
-	echo exec( "gitconfig ${username} ${email}" );
+	var_dump( shell_exec( "gitconfig ${username} ${email}" ) );
+	print_r( shell_exec( "gitconfig ${username} ${email}" ) );
+	echo shell_exec( "gitconfig ${username} ${email}" );
 }
