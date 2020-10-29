a2dd49e497c5e319b1c9a9b932eba2bca9910978
Varun <varunsridharan23@gmail.com>
2020-10-29 20:04:01 +0530

    Added Util.php

--- toolkit/php.php
+++ toolkit/php.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once __DIR__ . '/php/util.php';
 require_once __DIR__ . '/php/logger.php';
 require_once __DIR__ . '/php/log-functions.php';
 require_once __DIR__ . '/php/inputvar.php';
